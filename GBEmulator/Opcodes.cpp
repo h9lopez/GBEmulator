83eb0ccdfce514f2eff99d3f0f53da452c0d8d25
Huandari Lopez <hlopez137@gmail.com>
2017-04-12 12:52:06 -0700

    Further instruction implementation/change in handler signatures

--- GBEmulator/Opcodes.cpp
+++ GBEmulator/Opcodes.cpp
@@ -8,7 +8,7 @@ namespace OpcodeHandlers
 
 	/*******************************INSTRUCTION HANDLERS**************************/
 
-	bool opcode_handle_ld_sp_imm(RegisterBank *regs, unsigned int data)
+	void opcode_handle_ld_sp_imm(RegisterBank *regs, unsigned int data)
 	{
 		cout << hex << "LD SP, d16 handler called. Encoded data is " << data << endl;
 		cout << dec << "PC in regbank is currently at " << static_cast<int>(regs->PC.word) << endl;
@@ -16,15 +16,22 @@ namespace OpcodeHandlers
 		// Load immediate in data (2 bytes) into register SP
 		regs->SP.lo = LOW8_MASK(data);
 		regs->SP.hi = HIGH8_MASK(data);
-		return true;
 	}
 
-	bool opcode_handle_xor_a(RegisterBank *regs, unsigned int data)
+	void opcode_handle_xor_a(RegisterBank *regs, unsigned int data)
 	{
 		cout << hex << "XOR A handler called, data is " << data << endl;
 		cout << dec << "PC in regbank is currently at " << static_cast<int>(regs->PC.word) << endl;
-		cout << hex << "SP is " << static_cast<int>(regs->SP.word) << endl;
-		return true;
+		
+		// A = A ^ A
+		// Reduced to A = 0 b/c a reg XOR'd by itself is always 0
+		regs->A = 0;
 	}
 
+	void opcode_handle_ld_hl_imm(RegisterBank *regs, unsigned int data)
+	{
+		// Load imm in data (2 bytes) into register HL
+		regs->HL.lo = LOW8_MASK(data);
+		regs->HL.hi = HIGH8_MASK(data);
+	}
 }
--- GBEmulator/Opcodes.h
+++ GBEmulator/Opcodes.h
@@ -5,7 +5,7 @@
 #define LOW8_MASK(x) (x & 0xFF)
 #define HIGH8_MASK(x) ((x & 0xFF00) >> 8)
 
-typedef std::function<bool(RegisterBank *regs, unsigned int)> opcode_handle_func;
+typedef std::function<void(RegisterBank *regs, unsigned int)> opcode_handle_func;
 
 // (int, functionptr) = length of instruction (in bytes), function pointer to handler
 typedef std::tuple< int, opcode_handle_func > opcode_handler_info;
@@ -13,12 +13,14 @@ typedef std::tuple< int, opcode_handle_func > opcode_handler_info;
 namespace OpcodeHandlers
 {
 
-	bool opcode_handle_ld_sp_imm(RegisterBank *regs, unsigned int data);
-	bool opcode_handle_xor_a(RegisterBank *regs, unsigned int data);
+	void opcode_handle_ld_sp_imm(RegisterBank *regs, unsigned int data);
+	void opcode_handle_xor_a(RegisterBank *regs, unsigned int data);
+	void opcode_handle_ld_hl_imm(RegisterBank *regs, unsigned int data);
 
 	const std::map<uint8_t, opcode_handler_info> global_opcode_vtable =
 	{
-		{ 0x31, opcode_handler_info{ 3, opcode_handle_ld_sp_imm } },
-		{ 0xAF, opcode_handler_info{ 1, opcode_handle_xor_a } }
+		{ 0x31, { 3, opcode_handle_ld_sp_imm }	},
+		{ 0xAF, { 1, opcode_handle_xor_a }		},
+		{ 0x21, { 3, opcode_handle_ld_hl_imm }	}
 	};
 }
