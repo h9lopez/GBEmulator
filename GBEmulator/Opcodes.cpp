30fc0ed2b1a016f83e175b505e1034d6f80cc774
Huandari Lopez <hlopez137@gmail.com>
2017-04-14 11:34:26 -0700

    Added infrastructure for encompassing bit-checking instructions for multiple combos of bits/regs.

--- GBEmulator/Opcodes.cpp
+++ GBEmulator/Opcodes.cpp
@@ -11,29 +11,40 @@ using namespace std;
 //		   i.e. implying the little-endian format of the Gameboy
 void CPU::reg_store_immediate(Register *reg, unsigned int data)
 {
-	reg->lo = LOW8_MASK(data);
-	reg->hi = HIGH8_MASK(data);
+	reg->hi = LOW8_MASK(data);
+	reg->lo = HIGH8_MASK(data);
 }
 
 inline uint8_t* CPU::decode_register_bits(unsigned int data)
 {
 	switch (data) {
 		case 0x0:
+			cout << "\tRegister selecting B" << endl;
 			return &this->regs.BC.hi;
 		case 0x1:
+			cout << "\tRegister selecting C" << endl;
 			return &this->regs.BC.lo;
 		case 0x2:
+			cout << "\tRegister selecting D" << endl;
 			return &this->regs.DE.hi;
 		case 0x3:
+			cout << "\tRegister selecting E" << endl;
 			return &this->regs.DE.lo;
 		case 0x4:
+			cout << "\tRegister selecting H" << endl;
 			return &this->regs.HL.hi;
 		case 0x5:
+			cout << "\tRegister selecting L" << endl;
 			return &this->regs.HL.lo;
 		case 0x6:
+			cout << "ERROR: UNIMPLEMENTED - loading from (HL)" << endl;
 			// TODO: Load from mem into temp reg
+			return nullptr;
 		case 0x7:
+			cout << "\tRegister selecting A" << endl;
 			return &this->regs.A;
+		default:
+			cout << hex << "ERROR: register encoding is not of proper type (" << static_cast<int>(data) << ")" << endl;
 	}
 	return nullptr;
 }
@@ -119,19 +130,19 @@ void CPU::opcode_handle_bit_check(unsigned int data)
 	// NOTE: This is an all-encompassing method, i.e. we need to check second byte to
 	//		 check which register we're looking at/which bit we're checking
 	
-	// TODO: Implement efficient way to extract both target register and target bit
-	//		Actually, NOTE: Check that one colorful page with the things. Yeah. It's encoded
-	uint8_t* target_reg = this->decode_register_bits(data);
+	uint8_t* target_reg = this->decode_register_bits(data & 0x7);
 	int target_bit = (data & 0x38) >> 3;
 
-	// TODO: Finish check
-
+	uint8_t tempreg = *target_reg;
+	bool bit_on = (tempreg >> target_bit) & 0x1;
+	
+	cout << "\tBit " << (target_bit) << " is " << (bit_on ? "on" : "off") << endl;
 	// Flag modifications:
 	// z-> opposite of what bit 7 is
 	// n-> 0
 	// h-> 1
 	// z-> left alone
-	this->set_zero_flag(true);
+	this->set_zero_flag(!bit_on);
 	this->set_subtract_flag(false);
 	this->set_half_carry_flag(true);
 }
