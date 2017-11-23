2b7c94e071e64ad4edb80dd79a86aa502c39d7ac
Huandari Lopez <hlopez137@gmail.com>
2017-11-22 21:50:29 -0500

    Updated CPU to posthumously increase cycle count after opcode finished ops. Tests pass?!?!

--- GBEmulator/gb_typeutils.h
+++ GBEmulator/gb_typeutils.h
@@ -12,20 +12,22 @@ static const int BYTETYPE_SIZE = 8;
 
 typedef uint16_t Address;
 
-// Typedef for the return type of the opcode functions.
-// A tuple of the value of PC increment that should occur after
-// and the second value the number of cycles taken by the opcode
-typedef std::tuple<WordType, ByteType> OpResult;
-
-// The normal amount to increment PC by after each instruction
-static const WordType PC_INC_NORMAL = 1;
-
 // Enum for a return type that describes whether 
 // an opcodes cycle count should be that of its TAKEN or UNTAKEN count
 // This changes, for example, in the case of JR/JP
 typedef enum {
     CYCLE_TAKEN,
     CYCLE_UNTAKEN
-} CYCLE_ACTION;
+} CycleAction;
+
+// Typedef for the return type of the opcode functions.
+// A tuple of the value of PC increment that should occur after
+// and the second value the number of cycles taken by the opcode
+typedef std::tuple<WordType, CycleAction> OpResult;
+
+// The normal amount to increment PC by after each instruction
+static const WordType PC_INC_NORMAL = 1;
+
+
 
 #endif 
