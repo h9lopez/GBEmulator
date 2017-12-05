94fc7f9e7002c9cc22ddffc0adc35ad8d776542b
Huandari Lopez <hlopez137@gmail.com>
2017-12-04 22:26:23 -0500

    Added more tests for 0x11, 21, 31

--- GBEmulator_Test/GBEmulator_Test.gen.cpp
+++ GBEmulator_Test/GBEmulator_Test.gen.cpp
@@ -42,6 +42,60 @@ TEST_F(CPUOpcodeTest, opcode_test_0x1)
 	ASSERT_EQ(after.BC(), 0xcafe);
 }
 
+TEST_F(CPUOpcodeTest, opcode_test_0x11)
+{
+    SetUp({ 0x11, 0xbe, 0xca });
+
+    cpu.cycle();
+
+    RegBank after;
+    regs.copyCurrentState(after);
+
+    ASSERT_EQ(cpu.cycleCount(), 12);
+
+	ASSERT_EQ(after.flagZero(), snapshot.flagZero());
+	ASSERT_EQ(after.flagSubtract(), snapshot.flagSubtract());
+	ASSERT_EQ(after.flagHalfCarry(), snapshot.flagHalfCarry());
+	ASSERT_EQ(after.flagCarry(), snapshot.flagCarry());
+	ASSERT_EQ(after.DE(), 0xcabe);
+}
+
+TEST_F(CPUOpcodeTest, opcode_test_0x21)
+{
+    SetUp({ 0x21, 0xbe, 0xba });
+
+    cpu.cycle();
+
+    RegBank after;
+    regs.copyCurrentState(after);
+
+    ASSERT_EQ(cpu.cycleCount(), 12);
+
+	ASSERT_EQ(after.flagZero(), snapshot.flagZero());
+	ASSERT_EQ(after.flagSubtract(), snapshot.flagSubtract());
+	ASSERT_EQ(after.flagHalfCarry(), snapshot.flagHalfCarry());
+	ASSERT_EQ(after.flagCarry(), snapshot.flagCarry());
+	ASSERT_EQ(after.HL(), 0xbabe);
+}
+
+TEST_F(CPUOpcodeTest, opcode_test_0x31)
+{
+    SetUp({ 0x31, 0xbe, 0xba });
+
+    cpu.cycle();
+
+    RegBank after;
+    regs.copyCurrentState(after);
+
+    ASSERT_EQ(cpu.cycleCount(), 12);
+
+	ASSERT_EQ(after.flagZero(), snapshot.flagZero());
+	ASSERT_EQ(after.flagSubtract(), snapshot.flagSubtract());
+	ASSERT_EQ(after.flagHalfCarry(), snapshot.flagHalfCarry());
+	ASSERT_EQ(after.flagCarry(), snapshot.flagCarry());
+	ASSERT_EQ(after.SP(), 0xbabe);
+}
+
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
